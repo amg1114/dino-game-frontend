74e0781ad36e19303810a39be4f803152236d152
Alejandro Moreno <alejandromogil@gmail.com>
2025-04-13 21:08:57 -0500

    chore: format code and update configurations for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,65 @@
-import "./App.css";
-import logo from "./assets/logo.svg";
+import './App.css';
+import logo from './assets/logo.svg';
 function App() {
-    return (
-        <>
-            <main className="bg-body py-9 h-screen ">
-                <header className="container flex items-center justify-between pb-2 border-b border-b-placeholder">
-                    <figure className="w-40">
-                        <img src={logo} alt="Dinogame" />
-                    </figure>
-                    <h2>Style guide</h2>
-                </header>
-                <div className="container">
-                    <h1>Dinogame works</h1>
-                    <p className="">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero velit repellat explicabo aperiam cupiditate minus tempora natus
-                        alias deserunt commodi! Vel tempore sequi sapiente sint. Dicta distinctio aperiam, neque, <a href="#">facilis consequatur</a>{" "}
-                        quis tempore incidunt atque odio provident soluta suscipit. At eaque soluta fugit. Doloremque ducimus eum tenetur eaque
-                        perspiciatis alias?
-                    </p>
-                    <h2>Botónes</h2>
-                    <section className="space-y-6">
-                        <div className="flex gap-4 items-end">
-                            <button type="button" className="primary-button">
-                                Primary button
-                            </button>
-                            <button type="button" className="primary-button primary-button--sm">
-                                Primary sm button
-                            </button>
-                            <button type="button" className="primary-button primary-button--xs">
-                                Primary xs button
-                            </button>
-                        </div>
+  return (
+    <>
+      <main className="bg-body h-screen py-9">
+        <header className="border-b-placeholder container flex items-center justify-between border-b pb-2">
+          <figure className="w-40">
+            <img src={logo} alt="Dinogame" />
+          </figure>
+          <h2>Style guide</h2>
+        </header>
+        <div className="container">
+          <h1>Dinogame works</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero velit repellat explicabo aperiam cupiditate
+            minus tempora natus alias deserunt commodi! Vel tempore sequi sapiente sint. Dicta distinctio aperiam,
+            neque, <a href="#">facilis consequatur</a> quis tempore incidunt atque odio provident soluta suscipit. At
+            eaque soluta fugit. Doloremque ducimus eum tenetur eaque perspiciatis alias?
+          </p>
+          <h2>Botónes</h2>
+          <section className="space-y-6">
+            <div className="flex items-end gap-4">
+              <button type="button" className="primary-button">
+                Primary button
+              </button>
+              <button type="button" className="primary-button primary-button--sm">
+                Primary sm button
+              </button>
+              <button type="button" className="primary-button primary-button--xs">
+                Primary xs button
+              </button>
+            </div>
 
-                        <div className="flex gap-4 items-end">
-                            <button type="button" className="secondary-button">
-                                Secondary button
-                            </button>
-                            <button type="button" className="secondary-button secondary-button--sm">
-                                Secondary sm button
-                            </button>
-                            <button type="button" className="secondary-button secondary-button--xs">
-                                Secondary xs button
-                            </button>
-                        </div>
+            <div className="flex items-end gap-4">
+              <button type="button" className="secondary-button">
+                Secondary button
+              </button>
+              <button type="button" className="secondary-button secondary-button--sm">
+                Secondary sm button
+              </button>
+              <button type="button" className="secondary-button secondary-button--xs">
+                Secondary xs button
+              </button>
+            </div>
 
-                        <div className="flex gap-4 items-end">
-                            <button type="button" className="thertiary-button">
-                                Thertiary button
-                            </button>
-                            <button type="button" className="thertiary-button thertiary-button--sm">
-                                Thertiary sm button
-                            </button>
-                            <button type="button" className="thertiary-button thertiary-button--xs">
-                                Thertiary xs button
-                            </button>
-                        </div>
-                    </section>
-                </div>
-            </main>
-        </>
-    );
+            <div className="flex items-end gap-4">
+              <button type="button" className="thertiary-button">
+                Thertiary button
+              </button>
+              <button type="button" className="thertiary-button thertiary-button--sm">
+                Thertiary sm button
+              </button>
+              <button type="button" className="thertiary-button thertiary-button--xs">
+                Thertiary xs button
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
