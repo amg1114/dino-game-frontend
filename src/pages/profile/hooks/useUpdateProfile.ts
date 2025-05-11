228423ac4cdcc23f104ffa20a37021f2988e0173
Alejandro Moreno <alejandromogil@gmail.com>
2025-05-10 21:37:19 -0500

    feat: implement ProfileInfo component with form for user profile updates and validation handling

---
+++ src/pages/profile/hooks/useUpdateProfile.ts
@@ -0,0 +1,146 @@
+import { useEffect, useState } from 'react';
+import { z } from 'zod';
+
+import { useAlert } from '../../../hooks/useAlert';
+import { useAuth } from '../../../hooks/useAuth';
+import { Usuario } from '../../../models/user.interface';
+import { userSchema } from '../../../utils/zod/user.validators';
+
+export type ErrorUsuario = Record<keyof Usuario, string[]>;
+
+export function useUpdateProfile() {
+  const { showToast, showAlert } = useAlert();
+  const { updateUsuario, usuario } = useAuth();
+  const [updates, setUpdates] = useState<Partial<Usuario>>({});
+  const [errors, setErrors] = useState<ErrorUsuario>({} as ErrorUsuario);
+
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const key = name as keyof Usuario;
+
+    // Check if the value is empty and set the error message
+    if (usuario && usuario[key] !== value) {
+      // If the value is different from the original, set it in updates
+      setUpdates((prev) => {
+        const newValue = {
+          ...prev,
+          [key]: value,
+        };
+
+        return newValue;
+      });
+
+      return;
+    }
+
+    // If the value is the same as the original, remove it from updates
+    setUpdates(() => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [key]: _, ...cleanVal } = updates;
+      return cleanVal;
+    });
+  };
+
+  const handleUpdateProfile = async () => {
+    showAlert({
+      type: 'warning',
+      title: '¿Estás seguro?',
+      message: '¿Quieres guardar los cambios realizados en tu perfil?',
+      isConfirm: true,
+      onClose: (confirm) => {
+        if (!confirm) {
+          showToast({
+            type: 'info',
+            message: 'No se han guardado los cambios',
+            duration: 2000,
+          });
+          setUpdates({} as Partial<Usuario>);
+          return;
+        }
+
+        updateProfile();
+      },
+    });
+  };
+
+  const updateProfile = async () => {
+    if (!canSubmit) {
+      showToast({
+        type: 'error',
+        message: 'Por favor, completa todos los campos requeridos',
+        duration: 2000,
+      });
+      return;
+    }
+
+    const loadingAlert = showAlert({
+      type: 'loading',
+      title: 'Actualizando perfil',
+      message: 'Por favor, espere...',
+    });
+
+    try {
+      await updateUsuario(updates);
+      showToast({
+        type: 'success',
+        message: 'Perfil actualizado correctamente',
+        duration: 2000,
+      });
+    } catch (error) {
+      console.error('Error al actualizar el perfil:', error);
+      showToast({
+        type: 'error',
+        message: 'Error al actualizar el perfil',
+        duration: 2000,
+      });
+    } finally {
+      loadingAlert.close();
+    }
+  };
+
+  useEffect(() => {
+    const checkErrors = () => {
+      if (Object.keys(updates).length === 0) {
+        setErrors({} as ErrorUsuario);
+      }
+      console.log('updates', updates);
+
+      try {
+        userSchema.parse({
+          ...usuario,
+          ...updates,
+        });
+
+        setErrors({} as ErrorUsuario);
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          for (const issue of (error as z.ZodError).issues) {
+            const field = issue.path[0] as keyof Usuario;
+            const message = issue.message;
+
+            setErrors((prev) => {
+              const prevErrors = prev[field] || [];
+              const newErrors = [...prevErrors.filter((err) => err !== message), message];
+
+              return {
+                ...prev,
+                [field]: newErrors,
+              };
+            });
+          }
+        }
+      }
+    };
+
+    checkErrors();
+  }, [updates, usuario]);
+
+  useEffect(() => {
+    const isValid = Object.keys(updates).length > 0 && Object.keys(errors).length === 0;
+    setCanSubmit(isValid);
+  }, [updates, errors]);
+
+  return { handleUpdateProfile, handleChange, canSubmit, errors, updates, usuario };
+}
--- src/pages/profile/profileInfo/ProfileInfo.tsx
+++ src/pages/profile/profileInfo/ProfileInfo.tsx
@@ -1,8 +1,90 @@
+import { StyledInput } from '../../../components/StyledInput';
+import { StyledSelect } from '../../../components/StyledSelect';
+import { useUpdateProfile } from '../hooks/useUpdateProfile';
+import countries from 'world-countries';
+
 export function ProfileInfo() {
+  const { handleUpdateProfile, handleChange, canSubmit, errors, updates, usuario } = useUpdateProfile();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleUpdateProfile();
+  };
+
   return (
-    <div>
-      <h1>Profile Information</h1>
-      <p>This is the profile information page.</p>
-    </div>
+    <>
+      {usuario && (
+        <section className="space-y-4">
+          <h1>Mis datos personales</h1>
+          <form className="grid gap-8 md:grid-cols-2" onSubmit={(e) => handleSubmit(e)}>
+            <StyledInput
+              id="nombre"
+              label="Nombre"
+              type="text"
+              placeholder="Escribe tu nombre"
+              value={updates.nombre === undefined ? usuario.nombre : updates.nombre}
+              errors={errors?.nombre}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
+            <StyledInput
+              id="fechaNacimiento"
+              label="Fecha de nacimiento"
+              type="date"
+              placeholder="01/01/2000"
+              value={updates.fechaNacimiento === undefined ? usuario.fechaNacimiento : updates.fechaNacimiento}
+              errors={errors?.fechaNacimiento}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
+            <StyledSelect
+              id="pais"
+              options={countries.map((country) => ({ label: country.name.common, value: country.cca2 }))}
+              value={updates.pais === undefined ? usuario.pais : updates.pais}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              label="país"
+            />
+            <StyledSelect
+              id="sexo"
+              options={[
+                { label: 'Masculino', value: 'M' },
+                { label: 'Femenino', value: 'F' },
+                { label: 'Otro', value: 'D' },
+              ]}
+              value={updates.sexo === undefined ? usuario.sexo : updates.sexo}
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              label="género"
+            />
+            <div className="col-span-full">
+              <StyledInput
+                id="correo"
+                type="email"
+                placeholder="jhondoe@example.com"
+                value={updates.correo === undefined ? usuario.correo : updates.correo}
+                errors={errors?.correo}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                label="correo electrónico"
+              />
+            </div>
+            <footer className="col-span-full flex items-center justify-end gap-4">
+              <button type="submit" className="primary-button" disabled={!canSubmit}>
+                Guardar
+              </button>
+              <button type="button" className="secondary-button" disabled={!canSubmit}>
+                Cancelar
+              </button>
+            </footer>
+          </form>
+        </section>
+      )}
+    </>
   );
 }
