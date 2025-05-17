962df86f292e73fdea99e251460484be32265c2e
Alejandro Moreno <alejandromogil@gmail.com>
2025-05-16 19:01:30 -0500

    feat: enhance TypeScript configuration and add path aliases in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -14,6 +15,19 @@ export default defineConfig(({ mode }) => {
         },
       },
       cors: true,
+    },
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/components'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@partials': path.resolve(__dirname, 'src/partials'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@providers': path.resolve(__dirname, 'src/providers'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@models': path.resolve(__dirname, 'src/models'),
+      },
     },
   };
 });
