ec3a63729ac608ce5d8355e1feb0d2b38b02923f
Alejandro Moreno <alejandromogil@gmail.com>
2025-06-02 15:10:01 -0500

    feat(category): add CategoryIndex component and integrate category fetching

--- src/models/categoria.interface.ts
+++ src/models/categoria.interface.ts
@@ -1,6 +1,9 @@
+import { VideoGame } from './video-game.interface';
+
 export interface Categoria {
   id: number;
   titulo: string;
   descripcion: null;
   slug: string;
+  videoGames?: VideoGame[];
 }
