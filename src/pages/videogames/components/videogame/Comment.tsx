c1c3a4204fdefffac5eaa64529238c8985c7f33a
Alejandro Moreno <alejandromogil@gmail.com>
2025-06-15 20:26:14 -0500

    style(Comment): update styling for comment component

--- src/pages/videogames/components/videogame/Comment.tsx
+++ src/pages/videogames/components/videogame/Comment.tsx
@@ -1,9 +1,9 @@
 import { Comentario } from '../../../../models/comentario.interface';
 
-export function Comment({ comment, isLast = false }: { comment: Comentario; isLast?: boolean }) {
+export function Comment({ comment }: { comment: Comentario }) {
   return (
     <>
-      <div className={`w-full pb-2 mb-3${!isLast ? 'border-b-placeholder border-b-2' : ''}`}>
+      <div className={`bg-placeholder mb-3 w-full rounded p-3`}>
         <h4>
           <span className="text-green">&#64;</span>
           {comment.user.nombre}
